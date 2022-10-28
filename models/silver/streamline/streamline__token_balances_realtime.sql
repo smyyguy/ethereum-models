d8bd7dcd39022454c1b7a28fe1eed6c2371a381f
Austin <austin@flipsidecrypto.com>
2022-10-27 23:37:45 -0400

    remove limit

--- models/silver/streamline/streamline__token_balances_realtime.sql
+++ models/silver/streamline/streamline__token_balances_realtime.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_get_token_balances(object_construct('sql_source', '{{this.identifier}}','sql_limit',2000000))",
+        func = "{{this.schema}}.udf_get_token_balances(object_construct('sql_source', '{{this.identifier}}'))",
         target = "{{this.schema}}.{{this.identifier}}"
     )
 ) }}
